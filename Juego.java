d1a87b1f8bf357a378eff9a3441d49b942be8e17
Piotrek Budzowski <piobudzows@gmail.com>
2025-06-05 18:03:41 +0200

    Update Juego.java

--- Juego.java
+++ Juego.java
@@ -2,7 +2,6 @@ package CursoJava_Ahorcado;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Scanner;
 
 public class Juego {
 
@@ -23,7 +22,7 @@ public class Juego {
     }
 
     public void iniciarPartida() {
-        String tematica = Basicos.leerCadena("Introduce la temática para la palabra (ej: animales, frutas)");
+        String tematica = Basicos.leerCadena("Introduce la tematica para la palabra:");
         int dificultad = Basicos.leerEntero("Introduce la dificultad (1-5)");
 
         Palabra palabraDB = conector.getPalabraAleatoria(tematica, dificultad);
@@ -31,7 +30,7 @@ public class Juego {
         if (palabraDB != null) {
             this.palabraSecreta = palabraDB.getPalabra().toLowerCase();
             this.palabraActual = new StringBuilder();
-            System.out.println("¡Comienza el juego! La palabra tiene " + palabraSecreta.length() + " letras.");
+            System.out.println("La palabra tiene " + palabraSecreta.length() + " letras.");
             jugar();
         } else {
             System.out.println("No se encontraron palabras con esa temática y dificultad. Intenta de nuevo.");
@@ -43,40 +42,40 @@ public class Juego {
 
         while (!partidaTerminada) {
             mostrarEstadoJuego();
-            String entrada = Basicos.leerCadena("Introduce una letra o adivina la palabra completa").toLowerCase();
+            String entrada = Basicos.leerCadena("Introduce una letra").toLowerCase();
 
             if (entrada.length() == 1) { // El jugador ingresó una letra
                 char letra = entrada.charAt(0);
                 if (letrasAdivinadas.contains(letra) || letrasFallidas.contains(letra)) {
-                    System.out.println("Ya has intentado con esa letra. Prueba otra.");
+                    System.out.println("Ya has intentado con esa letra, espabila");
                 } else if (palabraSecreta.contains(String.valueOf(letra))) {
-                    System.out.println("¡Bien! La letra '" + letra + "' está en la palabra.");
+                    System.out.println("La letra '" + letra + "' está en la palabra.");
                     actualizarPalabraActual(letra);
                     letrasAdivinadas.add(letra);
                 } else {
-                    System.out.println("La letra '" + letra + "' no está en la palabra. Pierdes un intento.");
+                    System.out.println("La letra '" + letra + "' no esta en la palabra");
                     letrasFallidas.add(letra);
                     intentosRestantes--;
                 }
             } else if (entrada.length() > 1) { // El jugador intentó adivinar la palabra
                 if (entrada.equals(palabraSecreta)) {
                     palabraActual = new StringBuilder(palabraSecreta);
-                    System.out.println("¡Felicidades! Has adivinado la palabra.");
+                    System.out.println("Has adivinado la palabra");
                     partidaTerminada = true;
                 } else {
-                    System.out.println("Palabra incorrecta. Pierdes un intento.");
+                    System.out.println("Palabra incorrecta");
                     intentosRestantes--;
                 }
             } else {
-                System.out.println("Entrada inválida. Por favor, introduce una letra o una palabra.");
+                System.out.println("Por favor, introduce una letra o una palabra.");
             }
 
             if (palabraActual.toString().equals(palabraSecreta)) {
-                System.out.println("¡Felicidades! Has adivinado la palabra: " + palabraSecreta);
+                System.out.println("Has adivinado la palabra: " + palabraSecreta);
                 conector.guardarHistorial(idUsuario, palabraSecreta, true, (6 - intentosRestantes));
                 partidaTerminada = true;
             } else if (intentosRestantes <= 0) {
-                System.out.println("¡Oh no! Te has quedado sin intentos. La palabra secreta era: " + palabraSecreta);
+                System.out.println("Perdisteeee. La palabra secreta era: " + palabraSecreta);
                 conector.guardarHistorial(idUsuario, palabraSecreta, false, (6 - intentosRestantes));
                 partidaTerminada = true;
             }
